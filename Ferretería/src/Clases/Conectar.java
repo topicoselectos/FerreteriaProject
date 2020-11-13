5cffee2da48ca8e6233485ab450c024283284900
ed0909 <70719981+ed0909@users.noreply.github.com>
2020-11-13 10:36:29 -0600

    Se arregla la conexión con la base de datos de MySQL

--- Ferretería/src/Clases/Conectar.java
+++ Ferretería/src/Clases/Conectar.java
@@ -4,26 +4,41 @@ package Clases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author e09am
  */
 public class Conectar {
+    Connection conectar = null;
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost/ferreteriadb"+
+                    "?userTimezone=true&serverTimezone=UTC", "root", "Jimenez023.");  
+            System.out.println("Conexión exitosa.....");
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error de tipo: "+ e);
+            
+        }
+        return conectar;
+    }
     
-     Connection conectar = null;
-     
-     public Connection conexion() {
-         try {
-             Class.forName("com.mysql.jdbc.Driver");
-             conectar = DriverManager.getConnection("jdbc:mysql://localhost:3305/ferreteriadb", "root", "Jimenez023.");
-         } catch (Exception e) {
-             System.out.println(e.getMessage()); 
-         }
-         
-         return conectar;
-     }
+    public void Desconectar(){
+        conectar=null;
+        if(conectar==null){
+            System.out.println("El servicio se encuentra desconectado");
+        }
+    }
     
-    
-    
+    public static void main(String[] args) {
+        
+        Conectar con;
+        con = new Conectar();
+        con.conexion();
+    }
 }
+
+
